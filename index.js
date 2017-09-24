5d2366802d3f208ad9d2e6e9b41837f9b9ee9eb8
Martin Maly <maly@maly.cz>
2017-09-24 19:26:37 +0200

    Init

---
+++ index.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+const WebSocket = require('ws');
+
+const SerialPort = require('serialport');
+
+const wss = new WebSocket.Server({ port: 1311 });
+
+var port;
+
+wss.on('connection', function connection(ws, req) {
+  //console.log(req.url);
+  if (port) {return;}
+  if (req.url==="/_list") {
+  	SerialPort.list()
+  		.then(function(ports){
+  			console.log(ports);
+  			var pp = ports.map(function(port){if (port.comName.indexOf("/")<0) port.comName="/null/"+port.comName; return port})
+  			ws.send(JSON.stringify(pp))
+  		})
+  	return;
+  }
+  var urlSplit = req.url.split("/");
+  var comName = urlSplit[2];
+  if (urlSplit[1]!=="null") comName = "/"+urlSplit[1]+"/"+urlSplit[2];
+  var param = urlSplit[3];
+  var speed = parseInt(urlSplit[4]);
+  console.log("Opening",comName,speed);
+  port = new SerialPort(comName, {
+  		baudRate: speed
+	});
+
+  port.on("data", function (data) {
+  	var arr = Array.prototype.slice.call(data, 0)
+  	//console.log("SER IN", arr);
+  	ws.send(JSON.stringify(arr));
+  })
+
+  ws.on('message', function (message) {
+  	if (!port) return;
+  	var mesg = JSON.parse(message);
+    console.log('received', mesg);
+    if (mesg.key) {port.write(mesg.key);return;}
+    if (mesg.code) {port.write(String.fromCharCode(mesg.code));return;}
+    if (mesg.dtr!==undefined) {
+    	//console.log("DTR",mesg.dtr); 
+    	port.set({dtr:mesg.dtr});
+    	return;}
+    if (mesg.cts!==undefined) {
+    	//console.log("cts",mesg.cts); 
+    	port.set({cts:mesg.cts});
+    	return;}
+  });
+  ws.on('close', function (message) {
+    console.log('close: %s', message);
+    port.close(function(){port=null;})
+  });
+
+  //ws.send('something');
+});
